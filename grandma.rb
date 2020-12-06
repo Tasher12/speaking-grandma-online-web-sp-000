0d8101dc5e96e04ebd8b3358c4996613696ddd2f
authentic-pipe-8414 <tasher.stidham@outlook.com>
2020-12-06 14:25:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,8 +4,10 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
+    phrase.downcase
   else phrase.upcase
     return "NO, NOT SINCE 1938!"
+    phrase.downcase
   end
 end 
 # Whatever you say to grandma, she should respond with
