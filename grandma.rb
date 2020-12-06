819a39600b6c930efdef31f0274a874513e7a760
authentic-pipe-8414 <tasher.stidham@outlook.com>
2020-12-06 14:35:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,6 +7,7 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   else phrase.upcase
     return "NO, NOT SINCE 1938!"
+    
   end
 end 
 # Whatever you say to grandma, she should respond with
